014490cd1c5d37daf0d77330d483fa4a2b366eee
Frank Fella <frank.fella@lambdaschool.com>
2022-08-24 18:06:57 -0700

    FFella Push 08/24/2022 18:06:57

--- src/main/java/com/bloomtech/codealong/MagicEightBall/controller/MagicEightBallController.java
+++ src/main/java/com/bloomtech/codealong/MagicEightBall/controller/MagicEightBallController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.Timestamp;
 
 @RestController                        // This class contains RESTful controllers
-@RequestMapping(path="/magic8ball")    // Base path for all URLs this class handles is "/Magic8Ball"
+//@RequestMapping(path="/magic8ball")    // Base path for all URLs this class handles is "/Magic8Ball"
 public class MagicEightBallController {
 
     // Instantiate the Dao to be used to acquire data
@@ -25,13 +25,13 @@ public class MagicEightBallController {
      * @param theRequest - Will contain the JSON data passed through the request body
      * @return           - A Magic8BallResponse object
      */
-    @PostMapping(value="/ask")
+    @PostMapping(value="magic8ball/ask")
     public MagicEightBallResponse getResponse(@RequestBody MagicEightBallRequest theRequest) {
            // Log the request to the server log
-           logRequest("Request received via HTTP POST URL: /ask and question: " + theRequest.getQuestion());
+           logRequest("Request received via HTTP POST URL: /magic8ball/ask and question: " + theRequest.getQuestion());
 
            // Return a new response object with the question asked and answer from the Magic Eight Ball
-            return new MagicEightBallResponse(theRequest.getQuestion() ,theEightBall.getResponse());
+           return new MagicEightBallResponse(theRequest.getQuestion() ,theEightBall.getResponse());
     }
     /**
      * Handle HTTP Get requests for responses to questions
@@ -44,10 +44,11 @@ public class MagicEightBallController {
      * @param question - Will contain the question to be asked as a query parameter
      * @return         - A Magic8BallResponse object
      */
-    @GetMapping (value="/ask")
+    @GetMapping (value="magic8ball/ask")
     public MagicEightBallResponse getResponse(@RequestParam String question) {
+
         // Log the request to the server log
-        logRequest("Request received via HTTP POST URL: /ask and question: " + question);
+        logRequest("Request received via HTTP GET URL: /magic8ball/ask and question: " + question);
 
         // Return a new response object with the question asked and answer from the Magic Eight Ball
         return new MagicEightBallResponse(question ,theEightBall.getResponse());
